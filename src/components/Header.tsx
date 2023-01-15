c93dc9f90777cdcb3ff4a38717dd5f93a19ec272
Logan Dallalio <logandallalio@gmail.com>
2023-01-14 23:16:51 -0600

    Hero Section and Nav

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Raleway } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import Logo from '../assets/images/TaylorsLogo.png';
+import Image from 'next/image';
+
+const raleway = Raleway({
+    subsets: ['latin'],
+    weight:'400',
+    variable: '--font-raleway',
+  display: 'optional'})
+type Props = {}
+const Header = (props: Props) => {
+  return (
+      <header className={raleway.className}>
+          <div id='navbar'>
+              <Image alt='Taylors Logo' src={ Logo } />
+              <nav>
+                  <a href="#">
+                  <div className="nav-item">Our Story</div>
+                  </a>
+                   <a href="#">
+                  <div className="nav-item">Become A Partner</div>
+                  </a>
+                   <a href="#">
+                  <div className="nav-item">Contact</div>
+                  </a>
+                   <a href="#">
+                  <div className="nav-item">Donate</div>
+                  </a>
+              </nav>
+          </div>
+    </header>
+  )
+}
+
+export default Header
